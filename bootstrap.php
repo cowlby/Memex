db3b8e0d183ded8eb166ab7406f851eb47b6964f
Jose Prado <pradador@me.com>
2011-11-15 18:33:24 -0600

    Fixed case sensitive paths.

--- bootstrap.php
+++ bootstrap.php
@@ -10,7 +10,7 @@ use Silex\Extension\TwigExtension;
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-require_once __DIR__.'/vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/vendor/Symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Silex'   => __DIR__.'/vendor/Silex/src',
@@ -45,7 +45,7 @@ $app->register(new SymfonyBridgesExtension(), array(
 $app->register(new TwigExtension(), array(
     'twig.path' => $app['src.path'].'/views',
     'twig.options' => array('debug' => $app['debug'], 'strict_variables' => true),
-	'twig.class_path' => $app['vendor.path'].'/twig/lib',
+    'twig.class_path' => $app['vendor.path'].'/Twig/lib',
     'twig.configure' => $app->protect(function ($twig) use ($app) {
         if (!$app['debug']) {
             $twig->setCache($app['twig.cache.path']);
